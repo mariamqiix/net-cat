9efd7ab49eee5f7426fc562eace78ea139451659
Ruqaya Helal <rhelal@noreply.learn.reboot01.com>
2023-10-23 14:26:32 +0000

    Update 'main.go'

--- main.go
+++ main.go
@@ -9,88 +9,16 @@ import (
 	"time"
 )
 
-//To do list
-//Control connections quantity >>> need to be check - try to have 10 connection and more and see what happen?
-//same name issue!
-// print this ugly :
-//			_nnnn_
-//         dGGGGMMb
-//        @p~qp~~qMb
-//        M|@||@) M|
-//        @,----.JM|
-//       JS^\__/  qKL
-//      dZP        qKRb
-//     dZP          qKKb
-//    fZP            SMMb
-//    HZM            MMMM
-//    FqM            MMMM
-//  __| ".        |\dS"qML
-//  |    `.       | `' \Zq
-// _)      \.___.,|     .'
-// \____   )MMMMMP|   .'
-//      `-'       `--'
-
 type Client struct {
 	conn net.Conn
 	name string
 }
 
 var HistoryMessage []string
-
 var clients []Client
-var names []string
+var ClientsNames []string
 
 func main() {
-	Server()
-}
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	firstTime := false
-	name := ""
-	reader := bufio.NewReader(conn)
-	for {
-		if !firstTime {
-			conn.Write([]byte("Welcome to TCP-Chat!" + "\n"))
-			conn.Write([]byte("[ENTER YOUR NAME]: " + "\n"))
-			name, _ = reader.ReadString('\n')
-
-			if len(HistoryMessage) != 0 {
-				for _, message := range HistoryMessage {
-					conn.Write([]byte(message + "\n"))
-				}
-			}
-			PrintMessage(conn, name[:len(name)-1]+" has joined our chat...")
-
-			firstTime = true
-		}
-
-		message, err := reader.ReadString('\n')
-
-		if err != nil {
-			PrintMessage(conn, name[:len(name)-1]+" has left our chat...")
-			// Remove the client from the list of clients
-			for i, c := range clients {
-				if c.conn == conn {
-					clients = append(clients[:i], clients[i+1:]...)
-					break
-				}
-			}
-			return
-		}
-
-		if len(message[:len(message)-1]) != 0 {
-			currentTime := time.Now()
-
-			messageB := "[" + currentTime.Format("2006-01-02 15:04:05") + "][" + name[:len(name)-1] + "]:[" + message[:len(message)-1] + "]"
-			HistoryMessage = append(HistoryMessage, messageB)
-
-			PrintMessage(conn, messageB)
-		}
-	}
-}
-
-func Server() {
 	Port := "8989"
 
 	//should we check if the user use port number less than 4 digit or use charcters or letters?
@@ -105,6 +33,7 @@ func Server() {
 	if err != nil {
 		log.Fatal("Error starting the server:", err)
 	}
+
 	defer listener.Close()
 
 	fmt.Printf("Listening on the port :%s\n", Port)
@@ -119,6 +48,7 @@ func Server() {
 		client := Client{conn: conn}
 
 		if len(clients) == 10 {
+			conn.Write([]byte("Maximum number of clients reached. Connection rejected." + "\n"))
 			log.Println("Maximum number of clients reached. Connection rejected.")
 			conn.Close()
 			continue
@@ -126,6 +56,83 @@ func Server() {
 
 		clients = append(clients, client)
 		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	WlcLogo := []string{
+		"          _nnnn_",
+		"         dGGGGMMb",
+		"        @p~qp~~qMb",
+		"        M|@||@) M|",
+		"        @,----.JM|",
+		"       JS^\\__/  qKL",
+		"      dZP        qKRb",
+		"     dZP          qKKb",
+		"    fZP            SMMb",
+		"    HZM            MMMM",
+		"    FqM            MMMM",
+		"  __| \".        |\\dS\"qML",
+		"  |    `.       | `' \\Zq",
+		" _)      \\.___.,|     .'",
+		" \\____   )MMMMMP|   .'",
+		"      `-'       `--'",
+	}
+
+	reader := bufio.NewReader(conn)
+	firstTime := false
+	name := ""
+
+	for {
+		if !firstTime {
+			conn.Write([]byte("Welcome to TCP-Chat!" + "\n"))
+			for _, line := range WlcLogo {
+				conn.Write([]byte(line + "\n"))
+			}
+			conn.Write([]byte("[ENTER YOUR NAME]: " + "\n"))
+
+			name, _ = reader.ReadString('\n')
+
+			for !NameExitene(name) {
+				conn.Write([]byte("[!!! THIS NAME ALREADY EXIST, CHOSE ANOTHER NAME !!!]: " + "\n"))
+				name, _ = reader.ReadString('\n')
+			}
+
+			ClientsNames = append(ClientsNames, name)
+
+			if name == "" {
+				Exit(conn)
+				return
+			}
+
+			if len(HistoryMessage) != 0 {
+				for _, message := range HistoryMessage {
+					conn.Write([]byte(message + "\n"))
+				}
+			}
+
+			PrintMessage(conn, name[:len(name)-1]+" has joined our chat...")
+
+			firstTime = true
+		}
+
+		message, err := reader.ReadString('\n')
+
+		if err != nil {
+			PrintMessage(conn, name[:len(name)-1]+" has left our chat...")
+			Exit(conn)
+			return
+		}
+
+		if len(message[:len(message)-1]) != 0 {
+			currentTime := time.Now()
+
+			messageB := "[" + currentTime.Format("2006-01-02 15:04:05") + "][" + name[:len(name)-1] + "]:" + message[:len(message)-1]
+			HistoryMessage = append(HistoryMessage, messageB)
+
+			PrintMessage(conn, messageB)
+		}
 	}
 }
 
@@ -140,15 +147,22 @@ func PrintMessage(conn net.Conn, message string) {
 	}
 }
 
-func NamesValdation(conn net.Conn) string {
-	conn.Write([]byte("[ENTER YOUR NAME]: " + "\n"))
-	reader := bufio.NewReader(conn)
-	name, _ := reader.ReadString('\n')
-	for _, ClientName := range clients {
-		if ClientName.name == name[:len(name)-1] {
-			conn.Write([]byte("!!! THIS NAME ALREADY EXIST , CHOSE ANOTHER NAME !!!" + "\n"))
-			name = NamesValdation(conn)
+func NameExitene(name string) bool {
+	for _, Clientname := range ClientsNames {
+		if Clientname == name {
+			return false
 		}
 	}
-	return name[:len(name)-1]
+	return true
+}
+
+func Exit(conn net.Conn) {
+	// Remove the client from the list of clients
+	for i, c := range clients {
+		if c.conn == conn {
+			clients = append(clients[:i], clients[i+1:]...)
+			ClientsNames = append(ClientsNames[:i], ClientsNames[i+1:]...)
+			break
+		}
+	}
 }
